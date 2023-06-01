bbd9e2e1168688619dfb15240bedb93f7a9ef061
Winnie213190613 <127078970+Winnie213190613@users.noreply.github.com>
2023-06-01 15:33:25 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -196,3 +196,12 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+const email = document.getElementById("email");
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
